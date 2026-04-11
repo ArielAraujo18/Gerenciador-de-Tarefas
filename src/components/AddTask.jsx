28d9c52935dadf36833c3d337f84a1b5c8761b2c
ArielAraujo18 <arielaraujo120@gmail.com>
2026-04-11 16:03:19 -0300

    Update

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -8,7 +8,7 @@ function AddTask({onAddTaskSubmit}){
     return(
         <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
             <input type="text" placeholder="Digite o título da tarefa" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" value={title} onChange={(event) => setTitle(event.target.value)}/>
-            <input type="text" placeholder="Digite a descrição da tarefa" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" value={description} onChange={(event) => setDescripiton(event.target.value)}/>
+            <input type="text" placeholder="Digite a descrição da tarefa" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" value={description} onChange={(event) => setDescripton(event.target.value)}/>
             <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium" onClick={() => {
                 // verificar se o título e a descrição estão preenchidos
                 if (!title.trim() || !description.trim()){
@@ -17,7 +17,7 @@ function AddTask({onAddTaskSubmit}){
 
                 onAddTaskSubmit(title, description);
                 setTitle("");
-                setDescripiton("");
+                setDescripton("");
             }}>Adicionar</button>
         </div>
     )
